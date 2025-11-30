c77d406a7abe71855cf10e02b282a213fe14afa1
Albin123725 <107454411+Albin123725@users.noreply.github.com>
2025-11-30 15:31:36 +0530

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Minecraft server runs ONLY in Google Colab
+# Render is for monitoring only via monitor.py
+echo "Render monitoring service activated"
+echo "Minecraft server should be running in Google Colab"

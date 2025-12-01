b6d5b1d30c701294826123eb960242ed96cff40c
Albin123725 <107454411+Albin123725@users.noreply.github.com>
2025-12-02 00:53:48 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,3 +3,4 @@
 # Render is for monitoring only via monitor.py
 echo "Render monitoring service activated"
 echo "Minecraft server should be running in Google Colab"
+echo "Open the Colab notebook to start the Minecraft 24/7 server"
